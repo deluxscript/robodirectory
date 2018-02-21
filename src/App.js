03f97e906611a21fed14ec96e585f0c920032730
deluxscript <delsam94@yahoo.com>
2018-02-22 00:22:42 +0100

    robofriends search filter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+// import { robots } from './robots';
 
-const App = () => {
-	return(
-		<div className = "tc">
-			<h1>RoboFriends</h1>
-			<SearchBox />
-			<CardList  robots = {robots}/>
-		</div>
-	);
+class  App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value });
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response=> response.json())
+		.then(users=> {this.setState({ robots: users})});
+	}
+	render() {
+		const robotsResult = this.state.robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		});
+
+		if (this.state.robots.length === 0) {
+			return <h1 className = "tc">App Loading...</h1>
+		}
+		else{
+			return(
+				<div className = "tc">
+					<h1>RoboFriends</h1>
+					<SearchBox  searchChange =  {this.onSearchChange}/>
+					<CardList  robots = {robotsResult}/>
+				</div>
+			);
+		}
+	}
 }
 
 export default App;
